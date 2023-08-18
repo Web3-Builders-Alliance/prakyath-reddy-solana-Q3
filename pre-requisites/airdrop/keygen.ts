0c29f8c03a3132886c9c820d8f2377ad9845c2d5
PrakyathReddy <prakyathreddyk@gmail.com>
2023-08-18 12:43:41 +0530

    completed step 1

--- pre-requisites/airdrop/keygen.ts
+++ pre-requisites/airdrop/keygen.ts
@@ -1,1 +1,11 @@
 // here, we are going to generate ourselves a new keypair
+
+// import keypair from @solana/web3.js
+import { Keypair } from "@solana/web3.js";
+
+// generate a new keypair
+let kp = Keypair.generate();
+console.log(
+  `you've generated a new solana wallet! - ${kp.publicKey.toBase58()}`
+);
+console.log(`secret key: ${kp.secretKey}`);
---
+++ pre-requisites/airdrop/notes.md
@@ -0,0 +1,2 @@
+public address of generated wallet: 6E3i2UCHvW11MxFw91wCVkUvmDroEKvzwB8xV84PX8FJ
+to run a keygen.ts file, use command 'yarn keygen', since script is already included in package.json file
