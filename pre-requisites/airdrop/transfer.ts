b2d9ea4d2e6df6595261ac2ac5b39bf321e4c692
PrakyathReddy <prakyathreddyk@gmail.com>
2023-08-18 15:19:37 +0530

    completed step 4

--- pre-requisites/airdrop/transfer.ts
+++ pre-requisites/airdrop/transfer.ts
@@ -24,6 +24,11 @@ const connection = new Connection("https://api.devnet.solana.com");
 // use solana/web3.js to transfer 0.1 sol from 'from' to 'to' wallet
 (async () => {
   try {
+    // display initial balance in 'from' wallet
+    const balance = await connection.getBalance(from.publicKey);
+    console.log(`Your wallet balance is ${balance / LAMPORTS_PER_SOL} SOL`);
+
+    // create a test transaction to canlculate fees
     const transaction = new Transaction().add(
       SystemProgram.transfer({
         fromPubkey: from.publicKey,
@@ -35,20 +40,46 @@ const connection = new Connection("https://api.devnet.solana.com");
       await connection.getLatestBlockhash("confirmed")
     ).blockhash;
     transaction.feePayer = from.publicKey;
-    // Sign transaction, broadcast, and confirm
+
+    // calculate the cost of sending the transaction
+    const fee =
+      (
+        await connection.getFeeForMessage(
+          transaction.compileMessage(),
+          "confirmed"
+        )
+      ).value || 0;
+
+    // remove our transfer instruction and replace it with a transfer of the remaining balance
+    transaction.instructions.pop();
+
+    // now add the instruction with the correct lamports amount
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: from.publicKey,
+        toPubkey: to,
+        lamports: balance - fee,
+      })
+    );
+
+    // sign the transaction, broadcast it, and confirm it
     const signature = await sendAndConfirmTransaction(connection, transaction, [
       from,
     ]);
+
     console.log(
       `Success! Check out your TX here: https://explorer.solana.com/tx/${signature}?cluster=devnet`
     );
-    // display remaining balance in 'from' wallet
-    const balance = await connection.getBalance(from.publicKey);
-    console.log(`Your wallet balance is ${balance / LAMPORTS_PER_SOL} SOL`);
-    // display balance of 'to' wallet
-    const toBalance = await connection.getBalance(to);
+
+    // display final balance in 'from' wallet
+    const finalBalance = await connection.getBalance(from.publicKey);
     console.log(
-      `The recipient's wallet balance is ${toBalance / LAMPORTS_PER_SOL} SOL`
+      `Your final wallet balance is ${finalBalance / LAMPORTS_PER_SOL} SOL`
+    );
+    // display final balance in 'to' wallet
+    const finalBalanceTo = await connection.getBalance(to);
+    console.log(
+      `Your final wallet balance is ${finalBalanceTo / LAMPORTS_PER_SOL} SOL`
     );
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
