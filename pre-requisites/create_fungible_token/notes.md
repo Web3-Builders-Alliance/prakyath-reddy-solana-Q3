5d2f522ad3a023f8d8e9d732a67597efcf60ad02
PrakyathReddy <prakyathreddyk@gmail.com>
2023-08-20 23:03:14 +0530

    deposit spl token to vault

--- pre-requisites/create_fungible_token/notes.md
+++ pre-requisites/create_fungible_token/notes.md
@@ -4,7 +4,7 @@ source: https://spl.solana.com/token#creating-a-token-mint
 ts-node ./mnemonic_to_uint8array.ts  
 mint id: 4RKW1u4XXgQJd3PB8VYmHTarkmL1GT58TDSSak76uVnN  
 spl-token supply 4RKW1u4XXgQJd3PB8VYmHTarkmL1GT58TDSSak76uVnN - shows supply of our token  
-Your ata is: 3zvdsJY9YbfyYNA69kAR4NDDvXqgronMge8aPLjk8UnT  
+Your ATA is: 3zvdsJY9YbfyYNA69kAR4NDDvXqgronMge8aPLjk8UnT  
 Your mint txn id is: 3zvdsJY9YbfyYNA69kAR4NDDvXqgronMge8aPLjk8UnT  
 spl-token supply 4RKW1u4XXgQJd3PB8VYmHTarkmL1GT58TDSSak76uVnN: 10  
 JSON token standards - https://docs.metaplex.com/programs/token-metadata/token-standard
---
+++ pre-requisites/create_fungible_token/vault_deposit_spl.ts
@@ -0,0 +1,98 @@
+import { Connection, Keypair, SystemProgram, PublicKey } from "@solana/web3.js";
+import * as bs58 from "bs58";
+import {
+  Program,
+  Wallet,
+  AnchorProvider,
+  Address,
+  BN,
+} from "@project-serum/anchor";
+import { wba_vault, IDL } from "./wba_vault";
+import dotenv from "dotenv";
+import { Key } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  TOKEN_PROGRAM_ID,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
+dotenv.config();
+
+// declare our wallet secret key
+const MY_WALLET_SECRET_KEY = process.env.MY_WALLET_SECRET_KEY!;
+const secretKeyUint8Array = bs58.decode(MY_WALLET_SECRET_KEY);
+const keypair = Keypair.fromSecretKey(secretKeyUint8Array);
+
+// Create a devnet connection
+const connection = new Connection("https://api.devnet.solana.com");
+
+// Github account
+const github = Buffer.from("PrakyathReddy", "utf8");
+
+// Create our anchor provider
+const provider = new AnchorProvider(connection, new Wallet(keypair), {
+  commitment: "confirmed",
+});
+
+// Create our program
+const program = new Program<wba_vault>(
+  IDL,
+  "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address,
+  provider
+);
+
+// Create the PDA for our enrollment account
+const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
+const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
+  enrollment_seeds,
+  program.programId
+);
+const vaultState = new PublicKey(
+  "8oJB65i8gDMwMWTgk2mozdkeF2bMFQLSv6LcG4npAU5A"
+);
+const vaultAuth = PublicKey.findProgramAddressSync(
+  [Buffer.from("auth"), vaultState.toBuffer()],
+  program.programId
+)[0];
+const vault = PublicKey.findProgramAddressSync(
+  [Buffer.from("vault"), vaultAuth.toBuffer()],
+  program.programId
+)[0];
+const mint = new PublicKey("4RKW1u4XXgQJd3PB8VYmHTarkmL1GT58TDSSak76uVnN");
+
+// Execute our enrollment transaction
+(async () => {
+  try {
+    const owner_ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey
+    );
+
+    const vault_ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      vaultAuth,
+      true
+    );
+
+    const txhash = await program.methods
+      .depositSpl(new BN(1e6))
+      .accounts({
+        owner: keypair.publicKey,
+        ownerAta: owner_ata.address,
+        vaultAta: vault_ata.address,
+        tokenMint: mint,
+        vaultState: vaultState,
+        vaultAuth: vaultAuth,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([keypair])
+      .rpc();
+    console.log(`Success! Check out your TX here:
+    https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
+})();
