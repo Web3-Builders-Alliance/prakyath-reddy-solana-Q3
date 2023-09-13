9437617cb299530c09bf13693ee56d9aa9fcaa2a
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-13 12:19:56 +0530

    setting up folders. Updated the constants to determine length and space required for storage

--- amm/programs/amm/src/lib.rs
+++ amm/programs/amm/src/lib.rs
@@ -1,16 +1,40 @@
 use anchor_lang::prelude::*;
-use state;
+
+mod constants;
+mod contexts;
+mod errors;
+mod helpers;
+mod state;
+
+use contexts::*;
 
 declare_id!("3XaekuKBM4f4Tdaa4xMEpwZDf6aHinZCFDdv1MGKAPYE");
 
 #[program]
 pub mod amm {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        has_authority: bool,
+    ) -> Result<()> {
+        ctx.accounts.init(&ctx.bumps, seed, fee, has_authority)
+    }
+
+    pub fn update(ctx: Context<Update>, locked: bool) -> Result<()> {
+        ctx.accounts.update(locked)
+    }
+
+    pub fn deposit(
+        ctx.Context<Deposit>,
+        amount: u64,
+        max_x: u64,
+        max_y: u64,
+        expiration: i64,
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y, expiration)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
