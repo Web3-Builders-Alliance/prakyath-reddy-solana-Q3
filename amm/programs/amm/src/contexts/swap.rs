1fbec6950121623e77bc0a1cb2748f308d5146fb
PrakyathReddy <prakyathreddyk@gmail.com>
2023-09-13 17:54:20 +0530

    added context to withdraw and swap instructions

---
+++ amm/programs/amm/src/contexts/swap.rs
@@ -0,0 +1,142 @@
+use crate::errors:AmmError;
+use crate::state::Config;
+use crate::{assert_non_zero, assert_not_expired, asser_not_locked};
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{transfer_checked, Mint. TokenAccount, TokenInterface, Transfer},
+};
+use constant_product_curve::{ConstantProduct, LiquidityPair};
+
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub mint_x: Box<InterfaceAccount<'info, Mint>>,
+    pub mint_y: Box<InterfaceAccount<'info, Mint>>,
+    #[account(
+        mut,
+        associated_token::mint = config.mint_x,
+        associated_token::authority = auth,
+    )]
+    pub vault_x: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        mut,
+        associated_token::mint = config.mint_y,
+        associated_token::authority = auth,
+    )]
+    pub vault_y: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        seeds = [b"auth", config.key().as_ref()],
+        bump = config.auth_bump
+    )]
+    /// CHECK: this is fine, just used for signing
+    pub auth: UncheckedAccount<'info>,
+    /// TODO: remove authority from PDA seeds
+    #[account(
+        has_one = mint_x,
+        has_one = mint_y,
+        seeds = [
+            b"config",
+            config.key().as_ref(),
+            config.seed.to_le_bytes().as_ref()
+        ],
+        bump = config.config_bump
+
+    )]
+    pub config: Account<'info, Config>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Swap<'info> {
+    pub fn swap(&mut self, is_x: bool, amount: u64, min: u64, expiration: u64) -> Result<()> {
+        assert_not_locked!(self.config.locked);
+        assert_not_expired!(expiration);
+        assert_non_zero!([amount]);
+
+        let mut curve = ConstantProduct::init(
+            self.vault_x.amount,
+            self.vault_y.amount,
+            self.vault_x.amount,
+            self.config.fee,
+            None,
+        )
+        .map_err(AmmError::from)?;
+
+        let p = match is_x {
+            true => LiquidityPair::X,
+            false => LiquidityPair::Y,
+        };
+
+        let res = curve.swap(p, amount, min).map_err(AmmError::from)?;
+
+        assert_non_zero!([res.deposit, res.withdrawn]);
+        self.deposit_tokens(is_x, res.deposit)?;
+        self.withdraw_tokens(is_x, res.withdraw)
+    }
+
+    pub fn deposit_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+            false => (self.user_y.to_account_info(), self.vault_y.to_account_info()),)
+        };
+
+        let accounts = Transfer {
+            from,
+            to,
+            authority: self.user.to_account_info(),
+        };
+
+        let ctx = CpiContext::new(self.token_program.to_account_info(), accounts);
+
+        transfer_checked(ctx, amount, self.mint_x.decimals)
+    }
+
+    pub fn withdraw_tokens(&mut self, is_x: bool, amount: u64) -> Result<()> {
+        let (from, to) = match is_x {
+            true => (
+                self.vault_y.to_account_info(),
+                self.user_y.to_account_info(),
+            ),
+            false => (
+                self.user_x.to_account_info(),
+                self.vault_x.to_account_info(),
+            ),
+        };
+
+        let accounts = Transfer {
+            from.
+            to,
+            authority: self.auth.to_account_info(),
+        };
+
+        let config_key = self.config.key();
+
+        let seeds = &[&b"auth", config_key.as_ref() & [self.config.auth_bump]];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let ctx = Context::new_with_signer(self.token_program.to_account_info(), accounts, signer_seeds);
+
+        transfer_checked(ctx, amount, self.mint_y.decimals)
+    }
+}
